7234f879b0ba23aee2a634e1abb6cb41afb62283
Kristin Germeroth <kristin.germeroth@gmail.com>
2019-07-12 14:58:56 -0700

    added thorough comments

--- sortab.py
+++ sortab.py
@@ -31,40 +31,57 @@ def sort_ab(a, b):
 	ia = 0
 	ib = 0
 
+	# If both input lists are empty, just return the new_list
 	if a == [] and b == []:
 		return new_list
 
+	# if list a is blank but list b is not, add list b to new_list
 	elif a == [] and b != []:
 		new_list.extend(b)
 		return new_list
 
+	# if list b is blank but list a is not, add list a to new_list
 	elif a != [] and b ==[]:
 		new_list.extend(a)
 		return new_list
+
+	# if none of the lists are empty:
 	else:
+		# index counters for lists a and b
 		ia = 0
 		ib = 0
 
+		# keep the loop going until both lists have been fully iterated through
 		while ia < len(a) and ib < len(b):
 
+			# if list a has been fully been fully iterated through add the next 
+			# item in list b to new list and increment index for b
 			if ia == len(a)-1:
 				new_list.append(b[ib])
 				ib += 1
-
+			
+			# if list b has been fully been fully iterated through add the next 
+			# item in list a to new list and increment index for a
 			elif ib == len(b)-1:
 				new_list.append(a[ia])
 				ia += 1
 
+			# if the item in list a is less than item in list b, add to new list 
+			# and increment index for a
 			elif a[ia] < b[ib]:
 				new_list.append(a[ia])
 				ia += 1
 
+			# if the item in both lists are equal, add them both to the new list 
+			# and increment both index counters
 			elif a[ia] == b[ib]:
 				new_list.append(a[ia])
 				new_list.append(b[ib])
 				ia += 1
 				ib += 1
 
+			# if item in list b is less than item in list a, add to new list and
+			# increment index for b
 			else:
 				new_list.append(b[ib])
 				ib += 1
